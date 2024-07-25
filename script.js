1fdfe740e4a2bfd8434be1c43bb20c2f29fd7207
ben mccarthy <bmccarthy90@gmail.com>
2024-07-25 18:27:51 -0400

    cube is spinning

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { Wireframe } from 'three/examples/jsm/Addons.js';
 
 // SCENE
 const scene = new THREE.Scene()
@@ -9,24 +10,50 @@ const material = new THREE.MeshBasicMaterial({color: '#ff0000'})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// TRANSFORMS
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+
 // SIZES
 const sizes = {
-    width: 800,
-    height: 600
+    width: 600,
+    height: 400
 }
 
 // CAMERA
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+camera.lookAt(mesh.position)
 
 // CANVAS
 const canvas = document.querySelector("canvas.webgl")
 
 // RENDERER
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 
-renderer.render(scene, camera)
+// CLOCK
+const clock = new THREE.Clock()
 
+// ANIMATOR FUNCTION
+
+const animate = () => {
+    // Elapsed Time
+    const elapsedTime = clock.getElapsedTime();
+
+    // Update objects
+    mesh.rotation.y = elapsedTime 
+    
+    // Render
+    renderer.render(scene, camera)
+    
+    // Call animate on next frame
+    window.requestAnimationFrame(animate)
+}
+animate()
