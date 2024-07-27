3dba708af0ef7705c97383e86458539d9024fbaa
Ben McCarthy <bmccarthy90@gmail.com>
2024-07-27 16:35:48 -0400

    Commented out daily price change fetch and displayCoinHistory function. made cube change color (red, green) according to price

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 import * as THREE from 'three'
 
+// GLOBAL ANIMATION VARIABLES
+let cubeColor = { color: '' };
+let cubeSpin = 1;
+
 // SCENE
 const scene = new THREE.Scene()
 
 // CUBE
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: '#ff0000'})
+const material = new THREE.MeshBasicMaterial({ color: "white" })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+console.log("first color", material.color)
 
 // TRANSFORMS
 // mesh.rotation.x = Math.PI * 0.25
@@ -47,11 +52,11 @@ const animate = () => {
     const elapsedTime = clock.getElapsedTime();
 
     // Update objects
-    mesh.rotation.y = elapsedTime 
-    
+    mesh.rotation.y = elapsedTime
+
     // Render
     renderer.render(scene, camera)
-    
+
     // Call animate on next frame
     window.requestAnimationFrame(animate)
 }
@@ -67,24 +72,24 @@ const coinFilter = document.getElementById("dropdown-content")
 
 // API call to get coin data
 fetch("https://api.coincap.io/v2/assets")
-.then(response => {
-    if (response.ok) {
-        response.json().then(coinData => {
-            // console.log(coinData.data)
-            createCoinData(coinData.data)
-            // Display bitcoin as default
-            displayCoinData(coinData.data[0])
-        })
-    } else {
-        // maybe get this error to say something more specific to error
-        alert("ERROR")
-    }
-})
+    .then(response => {
+        if (response.ok) {
+            response.json().then(coinData => {
+                // console.log(coinData.data)
+                createCoinData(coinData.data)
+                // Display bitcoin as default
+                displayCoinData(coinData.data[0])
+            })
+        } else {
+            // maybe get this error to say something more specific to error
+            alert("ERROR")
+        }
+    })
 
 
 // FUNCTIONS
 
-function populateCoinList(cryptocurrency){
+function populateCoinList(cryptocurrency) {
     const coinListItem = document.createElement('li')
     coinListItem.textContent = `${cryptocurrency.symbol}: ${cryptocurrency.name}`
     coinList.appendChild(coinListItem)
@@ -100,38 +105,30 @@ function populateCoinList(cryptocurrency){
 
     // Click to add to MY COINS and DISPLAY COIN DATA
     coinListItem.addEventListener("click", () => {
-        // second API call for coin price history
-        fetch(`https://api.coincap.io/v2/assets/${cryptocurrency.id}/history?interval=d1`)
-        .then(response => response.json())
-        .then((coinHistory) => {
-            // access last two days of coin history
-            // assign array.length to a variable, reference variable instead of calling.length
-            const coinHistoryArray = coinHistory.data
-            // use pop to do this
-            const coinPriceTodayArrayLocation = coinHistoryArray.length -1
-            const coinPriceYesterdayArrayLocation = coinHistoryArray.length -2
-            // 
-            const coinPriceToday = coinHistoryArray[coinPriceTodayArrayLocation].priceUsd;
-            const coinPriceYesterday = coinHistoryArray[coinPriceYesterdayArrayLocation].priceUsd;
-            const coinChange = coinPriceToday - coinPriceYesterday;
-            // console.log("today",coinPriceToday)
-            // console.log("yesterday",coinPriceYesterday)
-            // console.log("change", coinChange)
-            displayCoinTrend(coinChange)
-        })
+        // second API call for coin price history, NEEDS WORK
+        // Maybe get price from 24hrs ago by hour?
+        // fetch(`https://api.coincap.io/v2/assets/${cryptocurrency.id}/history?interval=d1`)
+        //     .then(response => response.json())
+        //     .then((coinHistory) => {
+        //         displayCoinTrend(coinHistory)
+        //     })
 
         displayCoinData(cryptocurrency)
-    }) 
-   
+    })
+
 }
 
-function createCoinData (cryptocurrenciesArray){
+function roundToTwoDecimalPlace(number) {
+    return (Math.round(number * 10 ** 2) / 10 ** 2);
+}
+
+function createCoinData(cryptocurrenciesArray) {
     cryptocurrenciesArray.forEach((cryptocurrency) => {
         populateCoinList(cryptocurrency)
     })
 }
 
-function displayCoinData (cryptocurrency) {
+function displayCoinData(cryptocurrency) {
     console.log(cryptocurrency)
     const cryptoName = document.getElementById('name')
     const cryptoSymbol = document.getElementById('symbol')
@@ -141,18 +138,40 @@ function displayCoinData (cryptocurrency) {
     const coinPrice = Number(cryptocurrency.priceUsd)
     const coin24Hr = Number(cryptocurrency.changePercent24Hr)
 
-    function roundToTwoDecimalPlace (number) {
-        return (Math.round(number * 10 ** 2) / 10 **2);
-    }
-
     cryptoName.textContent = `NAME: ${cryptocurrency.name}`
     cryptoSymbol.textContent = `SYMBOL: ${cryptocurrency.symbol}`
     cryptoPrice.textContent = `PRICE USD: $${roundToTwoDecimalPlace(coinPrice)}`
     crypto24Hr.textContent = `24Hr CHANGE: ${roundToTwoDecimalPlace(coin24Hr)}%`
 
+    // CHANGE CUBE COLOR AND ROTATION
+
+    if (coin24Hr > 0) {
+        material.color.setColorName("green")
+    } else {
+        material.color.setColorName("red")
+    }
+    console.log("final color", material.color)
+
 }
 
-// LEFT OFF HERE ON FRIDAY
-function displayCoinTrend (coinChange) {
-    console.log(coinChange)
-}
+// DISPLAY PRICE HISTORY, NEEDS WORK
+// function displayCoinTrend(coinHistory) {
+//     // access last two days of coin history
+//     // assign array.length to a variable, reference variable instead of calling.length
+//     const coinHistoryArray = coinHistory.data
+//     // use pop to do this
+//     const coinPriceTodayArrayLocation = coinHistoryArray.length - 1
+//     const coinPriceYesterdayArrayLocation = coinHistoryArray.length - 2
+//     // 
+//     const coinPriceToday = coinHistoryArray[coinPriceTodayArrayLocation].priceUsd;
+//     const coinPriceYesterday = coinHistoryArray[coinPriceYesterdayArrayLocation].priceUsd;
+//     const coinChange = coinPriceYesterday - coinPriceToday;
+//     console.log("yesterday", coinPriceYesterday)
+//     console.log("today", coinPriceToday)
+
+//     console.log("change", coinChange)
+
+//     const priceYesterday = document.getElementById('price yesterday')
+//     priceYesterday.textContent = `PRICE YESTERDAY: $${roundToTwoDecimalPlace(coinPriceYesterday)}`
+//     console.log(priceYesterday)
+// }
